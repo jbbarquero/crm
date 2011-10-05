683cba6118ff3c1e6b0c4ec2cf8900606d8990da
Javier Beneito Barquero <jbbarquero@gmail.com>
2011-10-06 00:49:17 +0200

    Added DatabaseInforController and JSP. Version 0.1.0 for cloud deploy

---
+++ src/main/java/com/malsolo/crm/web/DatabaseInfoController.java
@@ -0,0 +1,45 @@
+package com.malsolo.crm.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/databaseinfo/**")
+@Controller
+public class DatabaseInfoController {
+	
+	@Autowired
+	DataSource dataSource;
+
+    @RequestMapping
+    public void get(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping
+    public String index(ModelMap modelMap) {
+    	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    	String userInfo = jdbcTemplate.queryForObject("select user", String.class);
+    	String urlInfo = "?";
+    	if (dataSource instanceof BasicDataSource) {
+    		urlInfo = ((BasicDataSource) dataSource).getUrl();
+    	}
+    	String versionInfo = jdbcTemplate.queryForObject("select version()", String.class);
+    	modelMap.put("userInfo", userInfo);
+    	modelMap.put("urlInfo", urlInfo);
+    	modelMap.put("versionInfo", versionInfo);
+    	return "databaseinfo/index";
+    }
+}
